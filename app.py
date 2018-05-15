056315bbe261091567f0f0694bcf2c0d4cecd46c
asutosh05 <asutosh407@gmail.com>
2018-05-15 23:19:20 +0530

    chat part is done

--- app.py
+++ app.py
@@ -6,31 +6,35 @@ import os,json
 app=Flask(__name__)
 
 #Using MongoDb Service provider Mlab free sandbox
-app.config['MONGO_DBNAME']='ps_time_table'
-app.config['MONGO_URI']='mongodb://devuser:sipan123.#@ds139262.mlab.com:39262/ps_time_table'
+app.config['MONGO_DBNAME']='dbname'
+app.config['MONGO_URI']='mongodb://YourUser:Yourpass@ds139262.mlab.com:39262/dbname'
 
 
 mongo = PyMongo(app)
 
 
 @app.route('/')
-def home_page():
-    # dist={}
-    # dist= mongo.db.census.aggregate([{"$group":{"_id":"$sex","count":{"$sum":1}}}])
+def home_page():    
     return render_template('index.html')
 
-@app.route('/grap')
-def getCount():
-    male_count = mongo.db.census.find({'sex':'Male'}).count()
-    female_count=mongo.db.census.find({'sex':'Female'}).count()
-    r = {'Male': male_count, 'Female': female_count}
-    return jsonify(json.dumps(r) )
+@app.route('/gender')
+def getMaleFemaleCount():
+    result= list(mongo.db.census.aggregate([{"$group":{"_id":"$sex","count":{"$sum":1}}}]))
+    countResult= {}
+    for res in result:
+        countResult[res.get('_id')] = res.get('count')
+    return jsonify(json.dumps(countResult))
+
+@app.route('/relationship')
+def getRelationshipCount():
+    result= list(mongo.db.census.aggregate([{"$group":{"_id":"$relationship","count":{"$sum":1}}}]))
+    countResult= {}
+    for res in result:
+        countResult[res.get('_id')] = res.get('count')
+    return jsonify(json.dumps(countResult))
 
 
-# if __name__ == '__main__':
-#   app.run()
+# app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
 
-app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
-
-# port = int(os.environ.get('PORT', 5000))
-# app.run(host="0.0.0.0", port=port, debug=True)
+port = int(os.environ.get('PORT', 5000))
+app.run(host="0.0.0.0", port=port, debug=True)
