c510f704e17a2f3aa035b79cace46a21e8d8dd19
asutosh05 <asutosh407@gmail.com>
2018-05-16 22:59:24 +0530

    almost done

--- app.py
+++ app.py
@@ -1,26 +1,36 @@
-from flask import Flask, request, render_template,jsonify
-from flask_pymongo import PyMongo
 import os,json
+from flask import Flask, request, render_template,jsonify
 from flask_caching import Cache
+from flask_pymongo import PyMongo
+
+
 
 
 app=Flask(__name__)
 
 #Using MongoDb Service provider Mlab free sandbox
-app.config['MONGO_DBNAME']='dbname'
-app.config['MONGO_URI']='mongodb://username:password@ds139262.mlab.com:39262/database'
+#Pymongo driver to connect mongodb
+app.config['MONGO_DBNAME']='ps_time_table'
+app.config['MONGO_URI']='mongodb://testdev:testdev@ds139262.mlab.com:39262/ps_time_table'
 
 #Initialize cache
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
 mongo = PyMongo(app)
 
-
+'''
+Main fuction to load census data ist fetching 10k data 
+Cashing for 100 sec to get the time to fech the data from db
+In that time use can use the appliction.
+'''
 @app.route('/')
+@cache.cached(timeout=100)
 def home_page():
-    data= list(mongo.db.census.find().limit(100))
+    data= list(mongo.db.census.find().limit(1000))
     return render_template('index.html',data=data)
 
+#To get the gender type  count
+#Converting json array to dist type
 @app.route('/gender')
 def getMaleFemaleCount():
     result= list(mongo.db.census.aggregate([{"$group":{"_id":"$sex","count":{"$sum":1}}}]))
@@ -29,6 +39,8 @@ def getMaleFemaleCount():
         countResult[res.get('_id')] = res.get('count')
     return jsonify(json.dumps(countResult))
 
+#To count relationship type count
+#Converting json array to dist type 
 @app.route('/relationship')
 def getRelationshipCount():
     result= list(mongo.db.census.aggregate([{"$group":{"_id":"$relationship","count":{"$sum":1}}}]))
